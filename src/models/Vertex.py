2ad5cdeedf6a92cbb936c2debc311976934c902b
Matheus de Lara <matheus.vex@gmail.com>
2023-01-02 20:40:52 -0300

    feat: modelando primeira letra do alfabeto]

--- src/models/Vertex.py
+++ src/models/Vertex.py
@@ -1,11 +1,16 @@
 class Vertex:
-  def __init__(self, x, y, z):
+  def __init__(self, x, y):
     self.x = x
     self.y = y 
-    self.z = z
   
   def coordinatesXY(self):
-    return self.x,self.y
+    return self.x, self.y
 
-  def coordinatesXYZ(self):
-    return self.x,self.y, self.z
+  def coordinatesY(self):
+    return self.y
+
+  def coordinatesX(self):
+    return self.x
+
+  # def coordinatesXYZ(self):
+  #   return self.x,self.y, self.z

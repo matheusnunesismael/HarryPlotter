2a44f176797b929dab82ee13a687fab05e15724c
Marcos Augusto Campagnaro <38305986+Kyhaiu@users.noreply.github.com>
2023-01-04 16:08:22 -0300

    Delete src directory

--- src/models/Edge.py
+++
@@ -1,7 +0,0 @@
-class Edge:
-  def __init__(self, startVertex, endVertex):
-    self.startVertex = startVertex
-    self.endVertex = endVertex
-
-  def calculateM(self):
-    self.m = ( self.endVertex.y - self.startVertex.y) / (self.endVertex.x - self.startVertex.x)
--- src/models/Face.py
+++
@@ -1,3 +0,0 @@
-class Face:
-  def __init__(self, edges):
-    self.edges = edges
